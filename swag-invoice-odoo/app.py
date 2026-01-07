0eb90267c7c9e4d8613d9b92678de8c746e7aee9
sabeya143111-arch <sabeya143111@gmail.com>
2026-01-07 10:53:36 +0300

    Update app.py

--- swag-invoice-odoo/app.py
+++ swag-invoice-odoo/app.py
@@ -8,7 +8,7 @@ from io import BytesIO
 
 
 # ========== Logo Display ==========
-st.image("https://raw.githubusercontent.com/sabeya143111-arch/swag-invoice-odoo/main/swag-invoice-odoo/logo.jpeg, width=250)
+st.image("https://raw.githubusercontent.com/sabeya143111-arch/swag-invoice-odoo/main/swag-invoice-odoo/logo.jpeg", width=250)
 st.markdown("---")
 
 st.set_page_config(
@@ -348,6 +348,7 @@ if uploaded_pdf is not None and convert_clicked:
         )
 elif uploaded_pdf is None:
     st.info("Upar se PDF select karo start karne ke liye.")
+
 
 
 
